03732b0c594ba58a419c88f969718bcf01c40398
cypriantingiweh <cypriantingiweh@gmail.com>
2019-10-12 04:39:47 -0700

    First commit

---
+++ src/main/java/com/first/springapp/firstwebspringapp/FirstWebSpringappApplication.java
@@ -0,0 +1,13 @@
+package com.first.springapp.firstwebspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebSpringappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstWebSpringappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first/springapp/firstwebspringapp/FirstWebSpringappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.first.springapp.firstwebspringapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstWebSpringappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
